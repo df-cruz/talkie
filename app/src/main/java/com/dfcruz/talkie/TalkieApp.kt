710afd44c9a546bcd1d585074aff4ceaaff3296e
dfcruz <dfcruz.dev@gmail.com>
2025-09-06 13:04:21 +0100

    Enable StrickMode in debug builds

--- app/src/main/java/com/dfcruz/talkie/TalkieApp.kt
+++ app/src/main/java/com/dfcruz/talkie/TalkieApp.kt
@@ -1,8 +1,28 @@
 package com.dfcruz.talkie
 
 import android.app.Application
+import android.os.StrictMode
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class TalkieApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(
+                StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyDeath()
+                    .build()
+            )
+            StrictMode.setVmPolicy(
+                StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyDeath()
+                    .build()
+            )
+        }
+    }
 }
